82b041e0f4840654b9d1a3246148507f12659c2f
Penn Curtis <penn.s.m.c@gmail.com>
2023-03-22 13:27:44 -0400

    complete

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+
+}
+
+function subtract() {
+    
+}
+
+function multiply() {
+    
+}
+
+function divide() {
+    
+}
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+let number = 1
+function increment(n) {
+    return (number += n)
+}
+
+let second = 1
+function decrement(n) {
+    return (n -= second)
+}
+
+function makeInt(n) {
+    let parsed = parseInt(n, 10)
+    return parsed
+}
+
+function preserveDecimal(n) {
+    let parsed = parseFloat(n)
+    return parsed
+}
